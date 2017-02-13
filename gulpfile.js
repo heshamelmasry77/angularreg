adffb8735afe4c97b6c366acc5186ff9a9d3bc72
heshamelmasry77 <heshamelmasry@outlook.com>
2017-02-13 11:16:34 +0200

    Installed gulp and gutil

---
+++ gulpfile.js
@@ -0,0 +1,7 @@
+var gulp = require('gulp');
+gutil = require('gulp-util');
+// TASK IS THE THINGS WE WANT IT TO GET DONE
+gulp.task('log', function() {
+    gutil.log('Workflows are awesome');
+
+});
