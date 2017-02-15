e465a989de4ec8e580dcf64d0604ff5c8444d7a1
heshamelmasry77 <heshamelmasry@outlook.com>
2017-02-15 12:37:13 +0200

    Adding Firebase registration to my controller
    i have added the Firebase registration to my registration controller

--- builds/development/js/controllers/registration.js
+++ builds/development/js/controllers/registration.js
@@ -1,11 +1,33 @@
-myApp.controller('RegistrationController', ['$scope', function($scope) {
-    // $scope.message = "Welcome to my App";
+myApp.controller('RegistrationController', ['$scope', '$firebase', '$firebaseAuth',
+    function($scope, $firebase, $firebaseAuth) {
+        // $scope.message = "Welcome to my App";
 
+        // create a reference to the database
+        var ref = firebase.database().ref();
+        // So this will be a reference to our database
 
-    $scope.login = function() {
-        $scope.message = "Welcome " + $scope.user.email;
-    };
-    $scope.register = function() {
-        $scope.message = "Welcome " + $scope.user.firstname;
-    };
-}]);
+        // another variable for the authentication part.
+        var auth = $firebaseAuth();
+
+        $scope.login = function() {
+            $scope.message = "Welcome " + $scope.user.email;
+        };
+        $scope.register = function() {
+
+            // so that we can register with an account
+            // a special function from Firebase called createUserWithEmailAndPassword
+
+            // then Firebase is going to give us a promise which you can get to with a then statement and it's going to return some registration information that we'll pass along into a variable called regUser.
+
+            auth.$createUserWithEmailAndPassword(
+                $scope.user.email,
+                $scope.user.password
+            ).then(function(regUser) {
+                $scope.message = "Hi " + $scope.user.firstname + ", Thank's for registering";
+
+            }).catch(function(error) {
+                $scope.message = error.message;
+            }); //createUserWithEmailAndPassword
+        }; //register
+    }
+]); //Controller
