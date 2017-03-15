5313162c415b08b0f61d7ea94fde45f86269221d
heshamelmasry77 <heshamelmasry@outlook.com>
2017-03-15 10:38:43 +0200

    Adding images icons

--- builds/development/js/app.js
+++ builds/development/js/app.js
@@ -1,35 +1,31 @@
 var myApp = angular.module('myApp', ['ngRoute', 'firebase']);
 
 // check for the authentication error event by adding something to myApp called run
-myApp.run(['$rootScope', '$location', function($rootScope, $location) {
-    $rootScope.$on('$routeChangeError', function(even, next, prevoius, error) {
+myApp.run(['$rootScope', '$location', function ($rootScope, $location) {
+    $rootScope.$on('$routeChangeError', function (even, next, prevoius, error) {
         if (error == 'AUTH_REQUIRED') {
             $rootScope.message = 'Sorry, you must log in to access that page';
             $location.path('/login');
         } //Auth Required
     }); //$routeChangeError
 }]); //run
-myApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+myApp.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
     $locationProvider.hashPrefix('');
-    $routeProvider.
-    when('/login', {
+    $routeProvider.when('/login', {
         templateUrl: 'views/login.html',
         controller: 'RegistrationController'
-    }).
-    when('/register', {
+    }).when('/register', {
         templateUrl: 'views/register.html',
         controller: 'RegistrationController'
-    }).
-    when('/success', {
+    }).when('/success', {
         templateUrl: 'views/success.html',
         controller: 'SuccessController',
         resolve: {
-            currentAuth: function(Authentication) {
+            currentAuth: function (Authentication) {
                 return Authentication.requireAuth();
             } //currentAuth
         } //resolve
-    }).
-    otherwise({
+    }).otherwise({
         redirectTo: '/login'
     });
 
